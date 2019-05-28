62b038c62b4058a2f100924116ed7793e2a791da
zeblade <predme@outlook.com>
2019-05-28 17:08:02 +0530

    song list

--- routes/getSong.js
+++ routes/getSong.js
@@ -3,19 +3,21 @@ var router = express.Router();
 
 router.post('/', function (req, res, next) {
     data = req.body;
+
     console.log(data,"df",userId,userAddress);
     CR.methods.getSong(data.sid,userId)
         .call({ from: userAddress }).then((val)=>{
           console.log(val);
           ipfs.files.get(val._hash,function (err,files) {
             files.forEach((file)=>{
-              fs.writeFile('aa.sol', file.content, function (err, file) {
+              fs.writeFile(data.sid+".mp3", file.content, function (err, file) {
                 if (err) throw err;
                   console.log('Saved!');
                   CR.methods.getUser(userId)
                       .call({ from:userAddress }).then((val) => {
                           console.log(val);
-                          res.render("main", {val : val});
+                          res.download("${__dirname}/"+data.sid+".mp3");
+                          // res.render("main", {val : val});
                       })
               });
 
--- routes/getUser.js
+++ routes/getUser.js
@@ -4,12 +4,29 @@ var router = express.Router();
 router.post('/', function (req, res, next) {
     data = req.body;
     songs =[];
-    // CR.methods.sCount.call().then(function)
+    songs2=[];
+    available = false;
     CR.methods.getSongIds.call().then(function(songIds){
       console.log("songIds",songIds);
+      if(songIds.length==0){
+        CR.methods.getUser(data.uid)
+            .call({ from: data.address }).then((val) => {
+                userAddress=data.address;
+                userId=data.uid;
+                res.render("main", {val : val,songs:songs,available_songs:songs2});
+            })
+      }
       for (var i = 0; i <= songIds.length; i++){
         CR.methods.songInfo(songIds[i]).call().then(function(song){
+          available=false;
+          CR.methods.checkPurchased(song[3],song[0]).call({from:userAddress}).then(function(available){
+            if (available) {
           songs.push({id:song[0],name:song[1],price:song[5],uid:song[3]});
+        }
+        else {
+          songs2.push({id:song[0],name:song[1],price:song[5],uid:song[3]});
+
+        }
           if(songs.length==songIds.length){
             CR.methods.getUser(data.uid)
                 .call({ from: data.address }).then((val) => {
@@ -18,6 +35,7 @@ router.post('/', function (req, res, next) {
                     res.render("main", {val : val,songs:songs});
                 })
           }
+        });
         })
       }
     })
