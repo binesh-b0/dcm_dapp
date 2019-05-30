6fc6f62ad52900a5dac6c32d7c22411816d3a77f
zeblade <predme@outlook.com>
2019-05-30 13:17:26 +0530

    designing

--- routes/buyLicense.js
+++ routes/buyLicense.js
@@ -4,9 +4,10 @@ var router = express.Router();
 router.post('/', function (req, res, next) {
     data = req.body;
     price=0;
-    CR.methods.songInfo(data.sid).call().then(function(song){
-      price = parseInt(song[4])+6000000;
-    CR.methods.buyLicense(userId,data.sid)
+    CR.methods.songInfo(data.sid).call({from:userAddress}).then(async (song) =>{
+      price = parseInt(song[5])+6000000;
+      console.log(song,price);
+   await CR.methods.buyLicense(userId,data.sid)
         .send({ from: userAddress,value:price, gas : 6000000 });
     res.send("license purchased !")
   });
--- routes/getUser.js
+++ routes/getUser.js
@@ -1,55 +1,30 @@
 var express = require('express');
 var router = express.Router();
+router.post('/',function (req, res, next) {
+  data = req.body;
+  userId = data.uid;
+  userAddress = data.address;
+  songs =[];
+  ar=[];
+  CR.methods.getUser(data.uid).call({ from: data.address }).then(async (val) => {
+    userAddress=data.address;
+    userId=data.uid;
+    ar=val[5].concat(val[6]);
+    console.log("ar",ar);
+    if (!ar.length) {
+      res.render("main", {val : val,s:songs});
+    }
+    for(i=0;i<(val[3]+val[4]);i++){
+      console.log(i);
+      await CR.methods.songInfo(parseInt(ar[i])).call({from:userAddress}).then(function(song){
+         songs.push({id:song[0],name:song[1],price:song[5],uid:song[3],hash:song[4]});
 
-router.post('/', function (req, res, next) {
-    data = req.body;
-    songs =[];
-    songs2=[];
-    available = false;
-    userId = data.uid;
-    userAddress = data.address;
-    CR.methods.getSongIds.call().then(function(songIds){
-      if(songIds.length==0){
-        CR.methods.getUser(data.uid)
-            .call({ from: data.address }).then((val) => {
-                userAddress=data.address;
-                userId=data.uid;
-                res.render("main", {val : val,songs:songs,songs2:songs});
+       }).catch((err)=>{console.log(err)})
 
-            })
-      }
-      for (var i = 0; i <= songIds.length; i++){
-        console.log(i,songIds,songs,songs2);
-        if(i>=songIds.length){
-          CR.methods.getUser(data.uid)
-              .call({ from: data.address }).then((val) => {
-                  userAddress=data.address;
-                  userId=data.uid;
-
-                  res.render("main", {val : val,songs:songs,songs2:songs2});
-              })
-        }
-        console.log("dd",songIds[i]);
-        CR.methods.songInfo(songIds[i]).call().then(function(song){
-          available=false;
-    
-          CR.methods.checkPurchased(userId,songIds[i]).call({from:userAddress}).then(function(available){
-            console.log(song[3],userId,available,"sdf");
-            if (available) {
-              console.log(available);
-          songs.push({id:song[0],name:song[1],price:song[5],uid:song[3],hash:song[4]});
-            }
-            else {
-              console.log(available);
-              songs2.push({id:song[0],name:song[1],price:song[5],uid:song[3]});
-            }
-            console.log(i,songIds.length);
-
-        });
-        })
-      }
-    })
-
-});
+    }
+    console.log("erebde",songs);
+      res.render("main", {val : val,s:songs});
+  })
+})
 
 module.exports = router;
